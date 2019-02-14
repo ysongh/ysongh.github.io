480e915986f46325271912187f42039beea852b5
song <yousonghou@yahoo.com>
2019-02-14 00:21:08 -0500

    add closeNav event listener that will close the nav menu when a nav link is clicked

--- js/main.js
+++ js/main.js
@@ -2,10 +2,13 @@ const navBtn = document.querySelector('.navigation__button');
 const nav = document.querySelector('.navigation__nav');
 const navList = document.querySelector('.navigation__list');
 const navItems = document.querySelectorAll('.navigation__item');
+const navLinks = document.querySelectorAll('.navigation__link')
 
 let showNav = false;
 
 navBtn.addEventListener('click', toggleNav);
+
+navLinks.forEach(link => link.addEventListener('click', closeNav));
 
 function toggleNav(){
     if(!showNav){
@@ -24,4 +27,13 @@ function toggleNav(){
 
         showNav = false;
     }
+}
+
+function closeNav(){
+    navBtn.classList.remove('close');
+    nav.classList.remove('show');
+    navList.classList.remove('show');
+    navItems.forEach(item => item.classList.remove('show'));
+
+    showNav = false;
 }
