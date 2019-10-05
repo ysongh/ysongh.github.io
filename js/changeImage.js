148ae0eabc75cd4aba1f8d2e8c00c63cef00297c
You Song Hou <yousonghou@yahoo.com>
2019-10-05 01:35:46 -0400

    add changeImage.js

---
+++ js/changeImage.js
@@ -0,0 +1,7 @@
+const image1 = document.getElementById("project1");
+
+image1.addEventListener('click', changeImage1);
+
+function changeImage1() {
+    document.getElementById("project1").src = "./img/project-3.PNG";
+}
