0b2b78b2776c254dc38dd486655f8376812a120d
You Song Hou <yousonghou@yahoo.com>
2019-10-05 20:18:50 -0400

    update changeImage1 function to switch images when clicked

--- js/changeImage.js
+++ js/changeImage.js
@@ -1,7 +1,23 @@
 const image1 = document.getElementById("project1");
 
+const image1List = [
+    "./img/project-B1.png",
+    "./img/project-B2.png",
+    "./img/project-B3.png",
+    "./img/project-B4.png"
+];
+
+let currentImage1 = 0;
+
 image1.addEventListener('click', changeImage1);
 
 function changeImage1() {
-    document.getElementById("project1").src = "./img/project-B2.png";
+    if(currentImage1 === image1List.length - 1){
+        currentImage1 = 0;
+    }
+    else{
+        currentImage1++;
+    }
+    
+    document.getElementById("project1").src = image1List[currentImage1];
 }
