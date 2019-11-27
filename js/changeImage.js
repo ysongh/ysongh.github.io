8da2bba719e0c425e117a9e9de22194aba1b47c7
You Song Hou <yousonghou@yahoo.com>
2019-11-27 01:28:27 -0500

    refactor the functions that change the project images

--- js/changeImage.js
+++ js/changeImage.js
@@ -1,7 +1,5 @@
-const nextImage1 = document.getElementById("nextProject1");
-const prevImage1 = document.getElementById("prevProject1");
-const nextImage2 = document.getElementById("nextProject2");
-const prevImage2 = document.getElementById("prevProject2");
+const nextImage = document.getElementById("nextProject");
+const prevImage = document.getElementById("prevProject");
 const modal = document.querySelector('.modal');
 const showModalP1Btn = document.getElementById('showModalP1Btn');
 const showModalP2Btn = document.getElementById('showModalP2Btn');
@@ -28,64 +26,59 @@ const image2ListLen = image2List.length - 1;
 let currentImage1 = 0;
 let currentImage2 = 0;
 
-let projectNumber = 0;
+let projectNumber = 1;
 
-if(nextImage1){
-    nextImage1.addEventListener('click', changeNextImage1);
-    prevImage1.addEventListener('click', changePrevImage1);
+if(nextImage){
+    nextImage.addEventListener('click', changeNextImage);
+    prevImage.addEventListener('click', changePrevImage);
     
     closeModalBtn.addEventListener('click', closeModal);
 }
 
-// nextImage2.addEventListener('click', changeNextImage2);
-// prevImage2.addEventListener('click', changePrevImage2);
-
-showModalP1Btn.addEventListener('click', showModal, "p1");
+showModalP1Btn.addEventListener('click', showModal);
 showModalP1Btn.myParam = "p1";
 showModalP2Btn.addEventListener('click', showModal);
 
-function changeNextImage1() {
-    if(currentImage1 === image1ListLen){
-        currentImage1 = 0;
+function changeNextImage(){
+    if(projectNumber === 1){
+        if(currentImage1 === image1ListLen){
+            currentImage1 = 0;
+        }
+        else{
+            currentImage1++;
+        }
+        document.getElementById("projects").src = image1List[currentImage1];
     }
     else{
-        currentImage1++;
+        if(currentImage2 === image2ListLen){
+            currentImage2 = 0;
+        }
+        else{
+            currentImage2++;
+        }
+        document.getElementById("projects").src = image2List[currentImage2];
     }
-    
-    document.getElementById("project1").src = image1List[currentImage1];
 }
 
-function changePrevImage1() {
-    if(currentImage1 === 0){
-        currentImage1 = image1ListLen;
+function changePrevImage(){
+    if(projectNumber === 1){
+        if(currentImage1 === 0){
+            currentImage1 = image1ListLen;
+        }
+        else{
+            currentImage1--;
+        }
+        document.getElementById("projects").src = image1List[currentImage1];
     }
     else{
-        currentImage1--;
+        if(currentImage2 === 0){
+            currentImage2 = image2ListLen;
+        }
+        else{
+            currentImage2--;
+        }
+        document.getElementById("projects").src = image2List[currentImage2];
     }
-    
-    document.getElementById("project1").src = image1List[currentImage1];
-}
-
-function changeNextImage2() {
-    if(currentImage2 === image2ListLen){
-        currentImage2 = 0;
-    }
-    else{
-        currentImage2++;
-    }
-    
-    document.getElementById("project2").src = image2List[currentImage2];
-}
-
-function changePrevImage2() {
-    if(currentImage2 === 0){
-        currentImage2 = image2ListLen;
-    }
-    else{
-        currentImage2--;
-    }
-    
-    document.getElementById("project2").src = image2List[currentImage2];
 }
 
 function showModal(pNumber){
