213ceea36db58741480c5100adb4ae1e7a16f2c0
You Song Hou <yousonghou@yahoo.com>
2020-02-05 00:34:42 -0500

    set up loading

--- js/loading.js
+++ js/loading.js
@@ -1,7 +1,11 @@
 const content = document.querySelector('.main-content');
+const loading = document.querySelector('.loading');
 
 function init(){
     setTimeout(() => {
+        loading.style.opacity = 0;
+        loading.style.display = "none";
+
         content.style.opacity = 1;
         content.style.display = "block";
     }, 1000);
