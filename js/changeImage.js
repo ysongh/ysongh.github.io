9429a4489826f685123d51c025bbc30ae1f77a5a
You Song Hou <yousonghou@yahoo.com>
2020-12-26 00:09:14 -0500

    add more images for Fund My Musician project

--- js/changeImage.js
+++ js/changeImage.js
@@ -4,6 +4,7 @@ const numProject = document.getElementById("numProject");
 const modal = document.querySelector('.modal');
 const showModalP1Btn = document.getElementById('showModalP1Btn');
 const showModalP2Btn = document.getElementById('showModalP2Btn');
+const showModalP3Btn = document.getElementById('showModalP3Btn');
 const closeModalBtn = document.getElementById('closeModalBtn');
 
 const image1List = [
@@ -19,11 +20,19 @@ const image2List = [
     "./img/project-B3.png"
 ];
 
+const image3List = [
+    "./img/project-C1.png",
+    "./img/project-C2.png",
+    "./img/project-C3.png"
+];
+
 const image1ListLen = image1List.length - 1;
 const image2ListLen = image2List.length - 1;
+const image3ListLen = image3List.length - 1;
 
 let currentImage1 = 0;
 let currentImage2 = 0;
+let currentImage3 = 0;
 
 let projectNumber = 1;
 
@@ -37,6 +46,8 @@ if(nextImage){
 showModalP1Btn.addEventListener('click', showModal);
 showModalP1Btn.myParam = "p1";
 showModalP2Btn.addEventListener('click', showModal);
+showModalP2Btn.myParam = "p2";
+showModalP3Btn.addEventListener('click', showModal);
 window.addEventListener('click', closeModal);
 
 function changeNextImage(){
@@ -50,7 +61,7 @@ function changeNextImage(){
         document.getElementById("projects").src = image1List[currentImage1];
         numProject.innerHTML = `${currentImage1 + 1}/${image1ListLen + 1} `;
     }
-    else{
+    else if(projectNumber === 2){
         if(currentImage2 === image2ListLen){
             currentImage2 = 0;
         }
@@ -59,6 +70,16 @@ function changeNextImage(){
         }
         document.getElementById("projects").src = image2List[currentImage2];
         numProject.innerHTML = `${currentImage2 + 1}/${image2ListLen + 1} `;
+    }
+    else{
+        if(currentImage3 === image3ListLen){
+            currentImage3 = 0;
+        }
+        else{
+            currentImage3++;
+        }
+        document.getElementById("projects").src = image3List[currentImage3];
+        numProject.innerHTML = `${currentImage3 + 1}/${image3ListLen + 1} `;
     }
 }
 
@@ -73,7 +94,7 @@ function changePrevImage(){
         document.getElementById("projects").src = image1List[currentImage1];
         numProject.innerHTML = `${currentImage1 + 1}/${image1ListLen + 1} `;
     }
-    else{
+    else if(projectNumber === 2){
         if(currentImage2 === 0){
             currentImage2 = image2ListLen;
         }
@@ -82,6 +103,16 @@ function changePrevImage(){
         }
         document.getElementById("projects").src = image2List[currentImage2];
         numProject.innerHTML = `${currentImage2 + 1}/${image2ListLen + 1} `;
+    }
+    else{
+        if(currentImage3 === 0){
+            currentImage3 = image3ListLen;
+        }
+        else{
+            currentImage3--;
+        }
+        document.getElementById("projects").src = image3List[currentImage3];
+        numProject.innerHTML = `${currentImage3 + 1}/${image3ListLen + 1} `;
     }
 }
 
@@ -93,10 +124,15 @@ function showModal(pNumber){
         document.getElementById("projects").src = image1List[currentImage1];
         numProject.innerHTML = `${currentImage1 + 1}/${image1ListLen + 1} `;
     }
-    else{
+    else if(pNumber.currentTarget.myParam === "p2"){
         projectNumber = 2;
-        document.getElementById("projects").src = image2List[currentImage2];
+        document.getElementById("projects").src = image2List[currentImage1];
         numProject.innerHTML = `${currentImage2 + 1}/${image2ListLen + 1} `;
+    }
+    else{
+        projectNumber = 3;
+        document.getElementById("projects").src = image3List[currentImage3];
+        numProject.innerHTML = `${currentImage3 + 1}/${image3ListLen + 1} `;
     }
 }
 
