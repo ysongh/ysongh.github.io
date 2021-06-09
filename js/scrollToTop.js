ed64ef5201e11cb3eec0361a7c7d89394b5251f7
You Song Hou <yousonghou@yahoo.com>
2021-06-08 23:54:59 -0400

    hide and show scroll to top button

--- js/scrollToTop.js
+++ js/scrollToTop.js
@@ -1,3 +1,16 @@
+const scrollBtn = document.getElementById("scrollToTopBtn");
+
+window.onscroll = function(){scrollFunction()};
+
+function scrollFunction() {
+  if(document.body.scrollTop > 500 || document.documentElement.scrollTop > 500){
+    scrollBtn.style.display = "block";
+  }
+  else{
+    scrollBtn.style.display = "none";
+  }
+}
+
 function scrollToTopOnClick() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
